151c63b274a317d0346e31e6a85028e0264028e6
amaliyamiriyeva8 <amaliyamiriyeva67>
2023-11-07 16:29:37 +0400

    js

---
+++ script.js
@@ -0,0 +1,43 @@
+//----------part-1-----------//
+// let Sherlock={
+// name:"Draco",
+// height:1.61,
+// wheight:46,
+
+// getInfo:function(){
+
+//    BKi=this.wheight/(this.height**2)
+//    return `BzKi is ${BKi}`;
+// }
+// }
+// console.log(Sherlock,Sherlock.getInfo())
+
+// //----------part-2-----------//
+// let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// console.log(bills);
+// let tips = [];
+// let totals = [];
+// let ortalma;
+
+// const funksiya = function(xoxo,ortalma) {
+
+//     for (let i = 0; i < xoxo.length; i++) {
+//         if (xoxo[i] > 50 && xoxo[i] <300) {
+//             tips.push(xoxo[i] * 0.15)
+//             totals.push(xoxo[i] + xoxo[i] * 0.15)
+//         }
+//         else {
+//             tips.push(xoxo[i] * 0.2)
+//             totals.push(xoxo[i] + xoxo[i] * 0.2)
+//         }
+//     }
+//     let a=0;
+//     for(let i=0; i<totals.length; i++){
+//        a=a+totals[i];
+
+//     }
+//     ortalma=a/totals.length;
+//     return (tips,totals,ortalma);
+// }
+// console.log(funksiya(bills))
+// 
